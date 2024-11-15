7ec6b1f095baa8f7cddee3bab768159cff448efc
Oliver Tacoronte <olivertm@gmail.com>
2024-11-15 11:57:15 +0000

    Se ha añadido comentarios

--- src/components/PrimerComponente.jsx
+++ src/components/PrimerComponente.jsx
@@ -27,9 +27,16 @@ export default function PrimerComponente() {
   }
 
   return (
+    /*
+    He reemplazado el div por el componente contenedor de Ionic: IonContent
+    */
     <IonContent>
       <h1>Esta es la llamada al primer componente</h1>
 
+        /*
+        He reempplazado el elemento <ul><li>BLA BLA BLA</li></ul>
+        Por el componente de Ionic: <IonList><IonItem><IonLabel>BLA BLA BLA</IonLabel></IonItem></IonList>
+        */
       <IonList>
         {dias_semana.map((value, index) => (
           <IonItem key={index}>
@@ -44,12 +51,17 @@ export default function PrimerComponente() {
       </p>
       <p>El nombre aplicado es: <strong>{updated}</strong></p>
       <p>Mi correo es: <strong>{email}</strong></p>
-
+        /*
+        He sustituido el input por el IonInput de Ionic
+        */
       <IonInput
         placeholder="Escribe tu nombre"
         value={nombre}
         onIonChange={nombreOnChange}
       ></IonInput>
+      /*
+      He sustituido el button por el IonButton de Ionic
+      */
       <IonButton onClick={buttonOnClick}>Actualizar nombre</IonButton>
     </IonContent>
   );
